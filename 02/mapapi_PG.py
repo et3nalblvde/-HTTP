ac40aef9ad2fa458a6092a6b966f7ade1fc571f0
et3nalblvde <arokii2001@gmail.com>
2025-03-02 13:52:48 +0300

    Полный поиск

---
+++ 02/mapapi_PG.py
@@ -0,0 +1,46 @@
+import pygame
+import requests
+import sys
+import os
+
+API_KEY_STATIC = 'f3a0fe3a-b07e-4840-a1da-06f18b2ddf13'
+
+def show_map(ll_spn=None, add_params=None):
+    if ll_spn:
+        map_request = f"https://static-maps.yandex.ru/v1?apikey={API_KEY_STATIC}&{ll_spn}"
+    else:
+        map_request = f"https://static-maps.yandex.ru/v1?apikey={API_KEY_STATIC}&"
+
+    if add_params:
+        map_request += "&" + add_params
+    response = requests.get(map_request)
+
+    if not response:
+        print("Ошибка выполнения запроса:")
+        print(map_request)
+        print("Http статус:", response.status_code, "(", response.reason, ")")
+        sys.exit(1)
+
+    map_file = "map.png"
+    try:
+        with open(map_file, "wb") as file:
+            file.write(response.content)
+    except IOError as ex:
+        print("Ошибка записи временного файла:", ex)
+        sys.exit(2)
+
+    pygame.init()
+    screen = pygame.display.set_mode((600, 450))
+    screen.blit(pygame.image.load(map_file), (0, 0))
+
+    pygame.display.flip()
+    while pygame.event.wait().type != pygame.QUIT:
+        pass
+
+    pygame.quit()
+
+    os.remove(map_file)
+
+
+if __name__ == '__main__':
+    show_map("ll=37.530887,55.703118&spn=0.002,0.002")
